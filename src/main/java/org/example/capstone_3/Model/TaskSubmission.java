003f559ce00e187d8c061678a417ac049484b6e3
Shahad Almalki <shahad.allmalki@gmail.com>
2026-06-03 13:13:55 +0300

    Update LearningGroupSevrice, TaskDTOOUT, Task, TaskService

--- src/main/java/org/example/capstone_3/Model/TaskSubmission.java
+++ src/main/java/org/example/capstone_3/Model/TaskSubmission.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Check(constraints = "status='SUBMITTED' or status='NOT SUBMITTED' or status='EVALUATED")
 public class TaskSubmission {
 
     @Id
@@ -22,8 +24,8 @@ public class TaskSubmission {
     @Column(columnDefinition = "TEXT")
     private String answerText;
 
-    @Column(columnDefinition = "boolean default false")
-    private Boolean completed;
+    @Column(columnDefinition = "varchar(80) not null")
+    private String status;
 
     @Column(columnDefinition = "int not null")
     private Integer score;
